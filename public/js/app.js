6cfa1c72063a11dea180f1a7ced4bc0f0de508f8
Balou <bluchier@laposte.net>
2020-08-22 15:32:32 +0200

    add hot reload

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/jean-kevin/Site/Velo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/jean-kevin/Site/Velo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/balou/site/Velo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/balou/site/Velo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,12 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                /*background-color: white;*/
                 /*color: #636b6f;*/
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
